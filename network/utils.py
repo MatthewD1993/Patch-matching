4e39643c22fe3b41c1453a61df1fd8c38535a2c3
Chengbiao Deng <ziyoububianmj@gmail.com>
2018-02-08 15:25:56 +0100

    New Compare Loss with old style customized Function, without ctx.

--- network/utils.py
+++ network/utils.py
@@ -67,6 +67,73 @@ class KITTI_3_Dataset(Dataset):
         return self.data[index]
 
 
+def compare_loss(input_p, input_n):
+    return CompareLoss()(input_p, input_n)
+
+
+class CompareLoss(Function):
+    def forward(self, input_p, input_n, t=0.1, size_average=True):
+        self.size_average = size_average
+
+        buffer = input_p.new()
+        buffer.resize_as_(input_p).copy_(input_p)
+        buffer = buffer - input_n + t
+        buffer[torch.lt(buffer, 0)] = 0
+
+        output = buffer.sum()
+        if self.size_average:
+            output = output / input_p.nelement()
+
+        self.delta = buffer
+
+        return input_p.new((output,))
+
+    def backward(self, grad_output):
+        size_average = self.size_average
+        delta = self.delta
+        grad_input_p = delta.new().resize_as_(delta)
+        grad_input_n = delta.new().resize_as_(delta)
+
+        grad_input_p.fill_(1)
+        grad_input_n.fill_(-1)
+
+        grad_input_p[torch.eq(delta, 0)] = 0
+        grad_input_n[torch.eq(delta, 0)] = 0
+
+        if size_average:
+            grad_input_p = grad_input_p / delta.nelement()
+            grad_input_n = grad_input_n / delta.nelement()
+
+        if grad_output[0] != 1:
+            grad_input_p.mul_(grad_output[0])
+            grad_input_n.mul_(grad_output[0])
+        return grad_input_p, grad_input_n, None, None
+
+    # @staticmethod
+    # @once_differentiable
+    # def backward(ctx, grad_output):
+    #     size_average = ctx.size_average
+    #     input_p, input_n, delta = ctx.saved_tensors
+    #     grad_input_p = input_p.new().resize_as_(input_p)
+    #     grad_input_n = input_n.new().resize_as_(input_n)
+    #
+    #     grad_input_p.fill_(1)
+    #     grad_input_n.fill_(-1)
+    #
+    #     grad_input_p[torch.lt(delta, 0)] = 0
+    #     grad_input_n[torch.lt(delta, 0)] = 0
+    #
+    #     if size_average:
+    #         grad_input_p = grad_input_p / input_p.nelement()
+    #         grad_input_n = grad_input_n / input_n.nelement()
+    #
+    #     if grad_output[0] != 1:
+    #         grad_input_p.mul_(grad_output[0])
+    #         grad_input_n.mul_(grad_output[0])
+    #     return grad_input_p, grad_input_n, None, None
+
+
+
 def new_hinge_loss(input, target, margin=1.0, t=0.3, size_average=True):
     return HingeEmbeddingLoss.apply(input, target, margin, t, size_average)
 
@@ -90,27 +157,22 @@ class HingeEmbeddingLoss(Function):
         buffer.add_(-t)
         buffer[torch.lt(buffer, 0)] = 0
         output = buffer.sum()
-        # print('pos', output)
 
         buffer.fill_(ctx.margin + ctx.t).add_(-1, input)
         buffer.clamp_(min=0)
-        # print('buffer', buffer)
         buffer[torch.eq(target, 1)] = 0
         output += buffer.sum()
-        # print('neg', output)
 
         if ctx.size_average:
             output = output / input.nelement()
-        # print("forward", output)
         ctx.save_for_backward(input, target)
         return input.new((output, ))
 
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output):
-        # print("grad_output", grad_output)
+
         input, target = ctx.saved_tensors
-        # print('input shape', input.shape)
         grad_input = input.new().resize_as_(input).copy_(target)
         grad_input[torch.mul(torch.eq(target, -1), torch.gt(input, ctx.margin+ctx.t))] = 0
         grad_input[torch.mul(torch.eq(target, 1), torch.lt(input, ctx.t))] = 0
@@ -166,7 +228,7 @@ def accuracy(pred, label, margin=1.0, t=0.3):
     acc[torch.mul(torch.eq(label, -1), torch.gt(pred, 0.5*(margin+t)))] = 1
     return torch.mean(acc)
 
-
+# TODO: Make this Function compatible with new pred format. (MoreSim network output.)
 def get_confuse_rate(pred, label=None):
     """Assume the order in a batch is well preserved.
     Then, pred [p, n, p, n, ...] if correct: pred[2*i] < pred[2*i + 1]
--- test_backward.py
+++ test_backward.py
@@ -1,28 +1,51 @@
 import torch
-from network.utils import new_hinge_loss
+from network.utils import new_hinge_loss, compare_loss
 
-def to_np(x):
-    return x.data.cpu().numpy()
+# def to_np(x):
+#     return x.data.cpu().numpy()
+#
+#
+# a_m = torch.autograd.Variable(torch.FloatTensor([0.1]), requires_grad=True)
+#
+#
+# for i in range(100):
+#     a = torch.Tensor([[0.2], [0.3]])
+#
+#     a_v = torch.autograd.Variable(a, requires_grad=False)
+#
+#     b = torch.FloatTensor([1, -1])
+#     b_v = torch.autograd.Variable(b, requires_grad=False)
+#
+#     c = torch.matmul(a_v, a_m)
+#     l_v = new_hinge_loss(c, b_v)
+#
+#     l_v.backward(retain_graph=True)
+#     a_m.data -= a_m.grad.data*0.1
+#     print('Iteration ', i)
+#     print('loss', to_np(l_v))
+#     print('a_m', to_np(a_m))
+#     print('gradient', to_np(a_m.grad))
+#     a_m.grad.zero_()
 
 
-a_m = torch.autograd.Variable(torch.FloatTensor([0.1]), requires_grad=True)
 
 
-for i in range(100):
-    a = torch.Tensor([[0.2], [0.3]])
+# # Test grad==========================================
 
-    a_v = torch.autograd.Variable(a, requires_grad=False)
-
-    b = torch.FloatTensor([1, -1])
-    b_v = torch.autograd.Variable(b, requires_grad=False)
-
-    c = torch.matmul(a_v, a_m)
-    l_v = new_hinge_loss(c, b_v)
-
-    l_v.backward(retain_graph=True)
-    a_m.data -= a_m.grad.data*0.1
-    print('Iteration ', i)
-    print('loss', to_np(l_v))
-    print('a_m', to_np(a_m))
-    print('gradient', to_np(a_m.grad))
-    a_m.grad.zero_()
+from network.utils import new_hinge_loss, accuracy
+import torch.nn.functional as F
+import torch
+from torch.autograd import Variable, gradcheck
+#
+p = Variable(torch.FloatTensor([0.5, 0.5]), requires_grad=True)
+n = Variable(torch.FloatTensor([0.55, 0.7]), requires_grad=True)
+#
+# # loss = new_hinge_loss(a, t)
+# loss = F.hinge_embedding_loss(a,t)
+# print(loss.data)
+# loss.backward()
+# print(a.grad)
+# print(t.grad)
+#
+test = gradcheck(compare_loss, (p, n), eps=1e-4, atol=1e-4)
+print(test)
