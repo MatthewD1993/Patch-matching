93ff2039a1eed0a0e6a784627a93b75b9a1c3a81
Chengbiao Deng <ziyoububianmj@gmail.com>
2018-03-22 21:29:56 +0100

    clean useless cpp file basicdefinitions.cpp.

--- src/basicdefinitions.cpp
+++
@@ -1,6 +0,0 @@
-#include "basicdefinitions.h"
-
-
-//
-//boost::uniform_real<float> _DEF_PROB::_rf(0.0000000, 0.999999);
-//boost::mt19937 _DEF_PROB::_rng;
--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,6 @@ extension_mod = Extension("patchselect",
 	include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs()+['/opt/Python-3.6.1/include/''/tmpbig/isg/opencv/modules/imgproc/include/', './'],
 	libraries = ['opencv_core', 'opencv_imgproc','opencv_highgui',],
 	library_dirs = ['/usr/local/lib'],
-	sources =["main.cpp",  "basicdefinitions.cpp",],
+	sources =["main.cpp" ],
 	extra_compile_args=['-std=c++11'])
 setup(name="patchselect", ext_modules=[extension_mod])
